d9a6b0118c39186480c2472bd285102a2109f450
puzzler <puzzledPublic@gmail.com>
2017-04-08 15:00:47 +0900

    Update mysql.js

--- config/mysql.js
+++ config/mysql.js
@@ -3,7 +3,7 @@ var mysql = require('mysql');
 var connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: '1575',
+    password: '',
     database: 'test'
 });
 connection.connect(function(err){
